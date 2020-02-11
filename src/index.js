a1a8ba971afae2bc81d43b43930faa12bc314bd9
Rahel Belay <rahelnbelay@gmail.com>
2020-02-11 10:55:21 -0500

    adds updates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+// {
+//     movieCount: 0
+// }
+
+// {
+//     type: 'NO_OF_MOVIES'
+// }
+
+// {
+//     lunch: "burrito"
+// }
+// {
+//     type: 'UPDATE_LUNCH_ITEM',
+//         preload: {
+//         itemName: "hot dog"
+
+//     }
+// }
+
+
 
 
 import {
@@ -26,26 +46,60 @@ function counter(state = defaultState, action) {
         case NOOFMOVIES:
             newState.movies[action.id] = state.movies[action.id] + action.howMuch
             break;
+        default:
         case CHANGESONG:
             newState.favSongs[action.id] = action.songName
+            break;
+
 
         case NOOFBURRITOS:
             newState.noOfBurritos[action.id] = state.noOfBurritos[action.id] + action.howMany
             break;
+
         case NOOFCOFFEE:
             newState.noOfCoffee[action.id] = state.noOfCoffee[action.id] + action.howMuch
             break;
-        case SANDWICH:
-            newState.sandwich.push(action.kind);
+
+        // case SANDWICH:
+        //     newState.sandwich.push(action.kind);
+        //     break;
+
 
     }
     return newState
 }
 
+function sandwich(state = defaultState, action) {
+    const newState = { ...state };
+
+    switch (action.type) {
+        case SANDWICH:
+            newState.sandwich.push(action.kind);
+            break;
+        default:
+            break;
+
+    }
+    return newState
+}
+
+
+
 const store = createStore(counter, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 store.subscribe(() => {
     console.table(store.getState());
 });
+
+const otherIntialState = {
+    sandwich: 'club Sandwich'
+}
+
+const store1 = createStore(sandwich,
+    otherIntialState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+
+
+
 
 function actionNoOfMovies(id, howMuch) {
     return {
@@ -84,7 +138,13 @@ function actionSancwichForLunch(kind) {
     }
 }
 
+window.store = store;
 
+
+store.subscribe(() => {
+    // just for dibbuging
+    console.table(store.getState());
+});
 
 
 store.dispatch(actionNoOfMovies(0, 1000))
@@ -92,6 +152,8 @@ store.dispatch(actionChangeSong(1, 'kkk'))
 store.dispatch(actionNoOfBurritos(0, 20))
 store.dispatch(actionNoOfCoffee(0, 20))
 store.dispatch(actionSancwichForLunch('bbb'))
+
+
 
 
 
